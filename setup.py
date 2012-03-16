b700ab21935ac4876ed090311c3fdeca70c9388f
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-03-16 10:47:50 +0400

    Two spaces indent.

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pywidl",
-    version = "0.0",
-    author = "Vasily Stepanov",
-    author_email = "vasily.stepanov@gmail.com",
-    description = "Generic code generator from WebIDL interfaces.",
-    install_requires = ['ply>=3.4'],
-    setup_requires = ['ply>=3.4'],
-    license = "MIT",
-    keywords = "webidl",
-    url = "https://github.com/VasilyStepanov/pywidl",
-    packages = ['pywidl', 'tests'],
-    long_description = read('README'),
-    classifiers = [
-        "Development Status :: 1 - Planning",
-        "Topic :: Software Development :: Code Generators",
-        "License :: OSI Approved :: MIT License",
-    ],
-    entry_points = """
-        [console_scripts]
-            foo = pywidl:foo
-    """
+  name = "pywidl",
+  version = "0.0",
+  author = "Vasily Stepanov",
+  author_email = "vasily.stepanov@gmail.com",
+  description = "Generic code generator from WebIDL interfaces.",
+  install_requires = ['ply>=3.4'],
+  setup_requires = ['ply>=3.4'],
+  license = "MIT",
+  keywords = "webidl",
+  url = "https://github.com/VasilyStepanov/pywidl",
+  packages = ['pywidl', 'tests'],
+  long_description = read('README'),
+  classifiers = [
+    "Development Status :: 1 - Planning",
+    "Topic :: Software Development :: Code Generators",
+    "License :: OSI Approved :: MIT License",
+  ],
+  entry_points = """
+    [console_scripts]
+      foo = pywidl:foo
+  """
 )
