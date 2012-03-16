185c1efb5451acc739d886422eb7a676df618088
Vasily Stepanov <vasily.stepanov@gmail.com>
2012-03-16 15:41:02 +0400

    real pywidl entry point.

--- pywidl/pywidl.py
+++ pywidl/pywidl.py
@@ -1,8 +1,63 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import sys
+
+def printUsage():
+  print "USAGE: pywidl <options> source"
+  print
+  print "  Options:"
+  print "    -m, --mako"
+  print "    -o, --output FILE"
+  print "    -t, --template FILE"
+  print
+
+
+
+class App(object):
+  MAKO_TEMPLATE = 0
+
+
+  def __init__(self, source, output, template, template_type):
+    self._source = source
+    self._output = output
+    self._template = template
+    self._template_type = template_type
+
+
+  def run(self):
+    print "hello world"
+
+
+
 def main():
-  print "hello world"
+  source = None
+  output = None
+  template = None
+  template_type = App.MAKO_TEMPLATE
+
+  i = 1
+  while i < len(sys.argv):
+    arg = sys.argv[i]
+    if arg == "-m" or arg == "--mako":
+      template_type = App.MAKO_TEMPLATE
+    elif arg == "-o" or arg == "--output":
+      i += 1
+      output = sys.argv[i]
+    elif arg == "-t" or arg == "--template":
+      i += 1
+      template = sys.argv[i]
+    else:
+      source = arg
+    i += 1
+
+  if source is None or output is None \
+  or template is None or template_type is None:
+    printUsage()
+    return
+
+  app = App(source, output, template, template_type)
+  return app.run()
 
 
 
